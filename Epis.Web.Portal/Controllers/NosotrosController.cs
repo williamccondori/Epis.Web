982833a2a5e4100dbe1e90200d6ff33ce3a4a40b
William Condori <williamccondori@outlook.com>
2017-12-02 21:01:44 -0500

    Agregar archivos de proyecto.

---
+++ Epis.Web.Portal/Controllers/NosotrosController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Epis.Web.Portal.Controllers
+{
+    public class NosotrosController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult PalabrasDirector()
+        {
+            return View();
+        }
+
+        public ActionResult MisionVision()
+        {
+            return View();
+        }
+
+        public ActionResult Objetivos()
+        {
+            return View();
+        }
+
+        public ActionResult PerfilProfesional()
+        {
+            return View();
+        }
+
+        public ActionResult ObjetivosEducacionales()
+        {
+            return View();
+        }
+
+        public ActionResult ResultadosAprendizaje()
+        {
+            return View();
+        }
+
+        public ActionResult Acreditacion()
+        {
+            return View();
+        }
+    }
+}
